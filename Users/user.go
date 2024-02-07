cfe1a1900b266ed6e1e3079312599761e0de79c2
FLOpeSS <sneto4413@gmail.com>
2024-02-07 15:44:23 -0300

    returning on func

--- Users/user.go
+++ Users/user.go
@@ -11,7 +11,16 @@ type User struct {
 	ValidPass bool
 }
 
-func (u *User) ValidatePassword() {
+func ThereIsUpper(s string) bool {
+	for _, i := range s {
+		if unicode.IsUpper(i) {
+			return true
+		}
+	}
+	return false
+}
+
+func (u *User) ValidatePassword() bool {
 	if len(u.Password) > 8 && ThereIsUpper(u.Password) {
 		fmt.Println("Valid password")
 		u.ValidPass = true
@@ -22,13 +31,5 @@ func (u *User) ValidatePassword() {
 		fmt.Println("Invalid password. Your password must countain at least a Uppercase char")
 		u.ValidPass = false
 	}
-}
-
-func ThereIsUpper(s string) bool {
-	for _, j := range s {
-		if unicode.IsUpper(j) {
-			return true
-		}
-	}
-	return false
+	return u.ValidPass
 }
